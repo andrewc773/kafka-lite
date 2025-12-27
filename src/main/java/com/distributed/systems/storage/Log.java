45a65befb9cd062193adc90f16a735e0cd079f9e
Andrew Chavarria <andrew.chavarria77@gmail.com>
2025-12-27 14:20:21 -0600

    implements Log class

---
+++ src/main/java/com/distributed/systems/storage/Log.java
@@ -0,0 +1,48 @@
+package com.distributed.systems.storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Manages high-level storage operations across segments.
+ */
+public class Log {
+    private final LogSegment activeSegment;
+    private final Path dataDir;
+
+    public Log(Path dataDir) throws IOException {
+        this.dataDir = dataDir;
+
+        //Ensure directory exists
+        if (!Files.exists(dataDir)) {
+            Files.createDirectories(dataDir);
+        }
+
+        // For now, initialize a single active segment
+        Path firstSegmentPath = dataDir.resolve("0.data");
+
+        this.activeSegment = new LogSegment(firstSegmentPath);
+    }
+
+    /*
+     * Append data to the current active segment.
+     * */
+    public long append(byte[] data) throws IOException {
+        return activeSegment.append(data);
+    }
+
+    /*
+     * Reads data based on a logical offset
+     * */
+    public byte[] read(long offest) throws IOException {
+        return activeSegment.read(offest);
+    }
+
+    /*
+     * Cleanly close the active segment and its index
+     * */
+    public void clean() throws IOException {
+        activeSegment.close();
+    }
+}
