acd67e601ec28370a430dcce9f846f4f38d57af0
Andrew Chavarria <andrew.chavarria77@gmail.com>
2026-02-06 20:28:19 -0600

    Updates server start logs

--- src/main/java/com/distributed/systems/Main.java
+++ src/main/java/com/distributed/systems/Main.java
@@ -13,10 +13,19 @@ public class Main {
 
             String dataDir = "kafka-logs";
 
-            System.out.println("Initializing Kafka-lite");
+            System.out.println("\u001B[36m" + "=".repeat(40));
+            System.out.println("       KAFKA-LITE BROKER v1.0");
+            System.out.println("=".repeat(40) + "\u001B[0m");
 
             // Internally creating log manager and socket
             BrokerServer server = new BrokerServer(port, dataDir);
+
+            System.out.printf("[BOOT] Port: %d%n", port);
+            System.out.printf("[BOOT] Data Directory: %s%n", Paths.get(dataDir).toAbsolutePath());
+            
+
+            System.out.println("\u001B[32m[READY] Broker is online and awaiting connections...\u001B[0m");
+            System.out.println("-".repeat(40));
 
             //stays alive w/ while(true) block
             server.start();
